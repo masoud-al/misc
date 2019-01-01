88a2aedcc749a20d6b6b00feeb03b0697fe84b3b
Masoud <mpoloton@gmail.com>
2019-01-01 20:53:11 +0200

    modified the hostname script

--- scripts/rpi-hostname/hostnames.sh
+++ scripts/rpi-hostname/hostnames.sh
@@ -5,21 +5,25 @@ HOST_PREFIX=${HOST_PREFIX:-"rpi"}
 
 for dev in $(ls /sys/class/net)
 do
-	if [[ "$dev" == *"lo"* ]]; then
-	  continue
-	fi
-	if [[ ! -z $(grep DEVTYPE=wlan /sys/class/net/$dev/uevent) ]]; then
- 	   continue
-	fi
-	MAC=$(cat /sys/class/net/$dev/address)
-	NET_DEVICE=$dev
+        if [[ "$dev" == *"lo"* ]]; then
+          continue
+        fi
+        if [[ ! -z $(grep DEVTYPE=wlan /sys/class/net/$dev/uevent) ]]; then
+           continue
+        fi
+        MAC=$(cat /sys/class/net/$dev/address)
+        NET_DEVICE=$dev
 done
 
 LAST_MAC4=$(sed -rn "s/^.*([0-9A-F:]{5})$/\1/gi;s/://p" /sys/class/net/$NET_DEVICE/address)
-NEW_HOSTNAME=${HOST_PREFIX}-${LAST_MAC4:-0000}
+HOSTNAME=${HOST_PREFIX}-${LAST_MAC4:-0000}
+PROPER_HOSTNAME=$(grep -i $MAC /boot/mac-names.txt | cut -f2)
 
-NEW_HOSTNAME=$(grep -i $MAC /boot/mac-names.txt | cut -f2)
+if [[ ! -z "$PROPER_HOSTNAME" ]]; then
+        HOSTNAME=$PROPER_HOSTNAME
+fi
 
-echo $NEW_HOSTNAME  > /etc/hostname
+echo $HOSTNAME  > /etc/hostname
 /bin/hostname -F /etc/hostname
+
 
